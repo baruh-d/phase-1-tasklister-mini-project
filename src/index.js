7fa0dc728eaddf2660f8dfc3236f48d0a50dd516
baru-d <barudaniel6@gmail.com>
2023-10-13 14:12:59 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+    // your code here
+  const inputValue = document.querySelector('#new-task-description');
+  const button = document.querySelector('#submit');
+  const list = document.querySelector('#tasks');
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (inputValue.value === '') {
+        alert('Please enter a task');
+      } else {
+      const liDOM = document.createElement('li');
+      const deleteButton = document.createElement('button');
+      deleteButton.innerText = 'X';
+      liDOM.innerText = inputValue.value;
+      liDOM.append(deleteButton);
+      list.append(liDOM);
+      inputValue.value = '';
+      deleteButton.addEventListener('click', (e) => {
+        e.target.parentNode.remove();
+        });
+      }
+    });
 });
